0e86fea7a3396d92c34fbd000e010c259dfadea9
Adib Izuddin <maizuddin35@gmail.com>
2018-01-16 22:40:38 +0800

    update comment for exercise 2

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,5 +1,7 @@
 //this is an unfinish program
 //finish up this program
+//take the value from x and y and sum it up.
+//this is a function related exercise, it need to be in a way where you put function in your program
 
 int sum(int x,int y)
 {
